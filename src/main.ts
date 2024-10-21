a2f84df9e8758e2ad153dddfd93bd2a47911f3a5
Demian Parkhomenko <95881717+DemianParkhomenko@users.noreply.github.com>
2024-10-21 16:30:33 +0300

    Add validation and error handling

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
